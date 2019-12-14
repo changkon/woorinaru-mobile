b01fb6493ae5ade252e3000248b5a6b3190dabb1
Changkon Han <chang_kon@hotmail.com>
2019-12-15 03:27:48 +0900

    Remove scrolling in list view

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -118,6 +118,7 @@ class _HomeState extends State<Home> {
   Widget _getPastEventsListWidget(List<Event> pastEvents) {
     return Expanded(
       child: ListView.builder(
+        physics: NeverScrollableScrollPhysics(),
         itemCount: pastEvents.length,
         itemBuilder: (BuildContext ctx, int index) {
           return EventCard(pastEvents[index]);
